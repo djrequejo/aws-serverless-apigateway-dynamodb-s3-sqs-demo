689c5f96205a3bf435e85b5a45dc36eb1f67ce34
Diego Requejo Torero <djrequejo@gmail.com>
2024-03-23 13:53:41 -0500

    feat(security): add an apikey and set It as private for the ' get-users' endpoint

--- generate-layer.sh
+++ generate-layer.sh
@@ -3,26 +3,26 @@
 echo "Step 1: Removing 'node_modules' folder if exists..."
 rm -rf node_modules
 
-echo "Step 2: Removing 'nodejs' folder if exists..."
-rm -rf app/nodejs
+echo "Step 2: Removing 'layers' folder if exists..."
+rm -rf layers
 
-echo "Step 3: Removing 'package-lock.json' and 'nodejs.zip' files if exists..."
-rm -f package-lock.json app/nodejs.zip
+echo "Step 3: Removing 'package-lock.json' files if exists..."
+rm -f package-lock.json
 
-echo "Step 4: Executing 'npm install --only=prod --arch=x64 --platform=linux --target=18x sharp'..."
-npm install --only=prod --arch=x64 --platform=linux sharp
+echo "Step 4: Executing 'npm install --production --arch=x64 --platform=linux --target=18x sharp'..."
+npm install --production --arch=x64 --platform=linux sharp
 
 echo "Step 5: Creating 'nodejs' folder..."
-mkdir app/nodejs
+mkdir layers/nodejs
 
 echo "Step 6: Copying 'node_modules' folder to 'nodejs'..."
-cp -r node_modules app/nodejs/
+cp -r node_modules layers/nodejs/
 
 echo "Step 7: Compressing 'nodejs' folder as 'nodejs.zip'..."
-zip -r nodejs.zip app/nodejs
+zip -r nodejs.zip layers/nodejs
 
 echo "Step 8: Removing 'nodejs' folder..."
-rm -rf app/nodejs
+rm -rf layers/nodejs
 
 echo "Step 9: Executing 'npm install'..."
 npm install
